5acbd900b9dc8577cbc20fdb61dd3367bd217286
Amila Welihinda <amilajack@gmail.com>
2018-05-05 11:01:34 -0700

    Removed images

--- demo/index.js
+++ demo/index.js
@@ -1,119 +1,117 @@
-var triangle = require('a-big-triangle')
-var createShader = require('gl-shader')
-var createFBO = require('gl-fbo')
-var glslify = require('glslify')
-var loop = require('raf-loop')
+import triangle from 'a-big-triangle';
+import createShader from 'gl-shader';
+import createFBO from 'gl-fbo';
+import glslify from 'glslify';
+import loop from 'raf-loop';
 
 
-
-var gl;
+let gl;
 
 function getBase64FromImageUrl(url) {
-    var img = new Image();
+  const img = new Image();
 
-    img.setAttribute('crossOrigin', 'anonymous');
+  img.setAttribute('crossOrigin', 'anonymous');
 
-    img.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width =this.width;
-        canvas.height =this.height;
+  img.onload = function () {
+    const canvas = document.createElement('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
 
-        console.log(this.width)
+    console.log(this.width);
 
-         gl = require('webgl-context')({
-            width: this.width,
-            height: this.height
-          })
-          document.body.appendChild(gl.canvas)
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
+    gl = require('webgl-context')({
+      width: this.width,
+      height: this.height
+    });
+    document.body.appendChild(gl.canvas);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(this, 0, 0);
 
-        var dataURL = canvas.toDataURL("image/png");
+    const dataURL = canvas.toDataURL('image/png');
 
-        const uri = dataURL;
-        // const uri = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    const uri = dataURL;
+    // const uri = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 
-        // var uri = require('./zoo')
-        var loadImage = require('load-img')
-        loadImage(uri, start)
+    // var uri = require('./zoo')
+    const loadImage = require('load-img');
+    loadImage(uri, start);
+  };
 
-    };
-
-    img.src = url;
+  img.src = url;
 }
 
-getBase64FromImageUrl('./demo/demo.jpg')
+getBase64FromImageUrl('./demo/demo.jpg');
 
-function start (err, image) {
-  if (err) throw err
+function start(err, image) {
+  if (err) throw err;
 
-  var width = gl.drawingBufferWidth
-  var height = gl.drawingBufferHeight
+  const width = gl.drawingBufferWidth;
+  const height = gl.drawingBufferHeight;
 
-  var texture = require('gl-texture2d')(gl, image)
+  const texture = require('gl-texture2d')(gl, image);
 
-  var shader = createShader(gl, glslify('./vert.glsl'), glslify('./frag.glsl'))
-  shader.bind()
-  shader.uniforms.iResolution = [width, height, 0]
-  shader.uniforms.iChannel0 = 0
+  const shader = createShader(gl, glslify('./vert.glsl'), glslify('./frag.glsl'));
+  shader.bind();
+  shader.uniforms.iResolution = [width, height, 0];
+  shader.uniforms.iChannel0 = 0;
 
-  var fboA = createFBO(gl, [width, height])
-  var fboB = createFBO(gl, [width, height])
+  const fboA = createFBO(gl, [width, height]);
+  const fboB = createFBO(gl, [width, height]);
 
   // apply linear filtering to get a smooth interpolation
-  var textures = [ texture, fboA.color[0], fboB.color[0] ]
-  textures.forEach(setParameters)
+  const textures = [texture, fboA.color[0], fboB.color[0]];
+  textures.forEach(setParameters);
 
-  var time = 0
+  let time = 0;
 
-  loop(render).start()
+  loop(render).start();
 
-  function render (dt) {
-    time += dt / 1000
-    gl.viewport(0, 0, width, height)
+  function render(dt) {
+    time += dt / 1000;
+    gl.viewport(0, 0, width, height);
 
-    var anim = (Math.sin(time) * 1.5)
+    const anim = (Math.sin(time) * 1.5);
     // var anim = (Math.sin(time) * 0.5 + 0.5)
-    var iterations = 8
-    var writeBuffer = fboA
-    var readBuffer = fboB
+    const iterations = 8;
+    let writeBuffer = fboA;
+    let readBuffer = fboB;
 
-    for (var i = 0; i < iterations; i++) {
+    for (let i = 0; i < iterations; i++) {
       // we will approximate a larger blur by using
       // multiple iterations starting with a very wide radius
-      var radius = (iterations - i - 1) * anim
+      const radius = (iterations - i - 1) * anim;
 
       // draw blurred in one direction
-      writeBuffer.bind()
+      writeBuffer.bind();
       if (i === 0) {
-        texture.bind()
+        texture.bind();
       } else {
-        readBuffer.color[0].bind()
+        readBuffer.color[0].bind();
       }
-      shader.bind()
-      shader.uniforms.flip = true
-      shader.uniforms.direction = i % 2 === 0 ? [radius, 0] : [0, radius]
-      gl.clearColor(0, 0, 0, 0)
-      gl.clear(gl.COLOR_BUFFER_BIT)
-      triangle(gl)
+      shader.bind();
+      shader.uniforms.flip = true;
+      shader.uniforms.direction = i % 2 === 0 ? [radius, 0] : [0, radius];
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      triangle(gl);
 
       // swap buffers
-      var t = writeBuffer
-      writeBuffer = readBuffer
-      readBuffer = t
+      const t = writeBuffer;
+      writeBuffer = readBuffer;
+      readBuffer = t;
     }
 
     // draw last FBO to screen
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
-    writeBuffer.color[0].bind()
-    shader.uniforms.direction = [0, 0] // no blur
-    shader.uniforms.flip = iterations % 2 !== 0
-    triangle(gl)
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    writeBuffer.color[0].bind();
+    shader.uniforms.direction = [0, 0]; // no blur
+    shader.uniforms.flip = iterations % 2 !== 0;
+    triangle(gl);
   }
 
-  function setParameters (texture) {
+  function setParameters(texture) {
     // texture.wrapS = texture.wrapT = gl.REPEAT
-    texture.minFilter = gl.LINEAR
-    texture.magFilter = gl.LINEAR
+    texture.minFilter = gl.LINEAR;
+    texture.magFilter = gl.LINEAR;
   }
 }
--- tools/coefficients.js
+++ tools/coefficients.js
@@ -1,50 +1,43 @@
-var range = require('array-range')
+import range from 'array-range';
 
-module.exports = coefficients
+export default coefficients;
 
-//a row of a pascal table with ends chopped off
+// a row of a pascal table with ends chopped off
 function coefficients(weights) {
-  if (weights.length % 2 === 0)
-    throw new Error('only supports odd table')
+  if (weights.length % 2 === 0) { throw new Error('only supports odd table'); }
 
-  console.log('taps', weights.length)
-  var mid = Math.floor((weights.length-1)/2)
+  console.log('taps', weights.length);
+  const mid = Math.floor((weights.length - 1) / 2);
 
-  var sum = weights.reduce(function(a, b) {
-    return a + b
-  }, 0)
+  const sum = weights.reduce((a, b) => a + b, 0);
 
-  weights = weights.slice(mid)
+  weights = weights.slice(mid);
 
-  var weightDiv = weights.map(function(w) {
-    return w / sum
-  })
+  const weightDiv = weights.map(w => w / sum);
 
-  var offsets = range(mid+1)
-  
-  var linearWeights = weightDiv.slice(0, 1)
-  var linearOffsets = [0]
-  for (var i=1; i<offsets.length-1; i+=2) {
-    var off1 = offsets[i]
-    var off2 = offsets[i+1]
-    var weight1 = weights[i]
-    var weight2 = weights[i+1]
+  const offsets = range(mid + 1);
 
-    var wsum = weight1 + weight2
-    var t = (off1 * weight1 + off2 * weight2) / wsum
-    linearOffsets.push(t)
-    linearWeights.push(wsum / sum)
+  const linearWeights = weightDiv.slice(0, 1);
+  const linearOffsets = [0];
+  for (let i = 1; i < offsets.length - 1; i += 2) {
+    const off1 = offsets[i];
+    const off2 = offsets[i + 1];
+    const weight1 = weights[i];
+    const weight2 = weights[i + 1];
+
+    const wsum = weight1 + weight2;
+    const t = (off1 * weight1 + off2 * weight2) / wsum;
+    linearOffsets.push(t);
+    linearWeights.push(wsum / sum);
   }
-  console.log('offsets', linearOffsets)
-  console.log('weights', linearWeights)
+  console.log('offsets', linearOffsets);
+  console.log('weights', linearWeights);
 }
 
-var weights = '28    56    70    56    28' // 5 taps
+const weights = '28    56    70    56    28' // 5 taps
 // var weights = '45    120   210   252   210   120   45' // 7 taps
 // var weights = '66    220    495    792    924    792    495    220    66' // 9 taps
 // var weights = '120    560    1820    4368    8008    11440    12870    11440    8008    4368    1820    560    120'
   .split(/\s+/)
-  .map(function(a) {
-    return parseInt(a, 10)
-  })
-module.exports(weights)
+  .map(a => parseInt(a, 10));
+module.exports(weights);
