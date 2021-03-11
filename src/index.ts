8385c7a2063f811bab5d2ae744e16a32ebd606be
Amila Welihinda <amilajack@gmail.com>
2021-03-10 17:09:29 -0800

    feat: migrate to class

--- src/index.ts
+++ src/index.ts
@@ -2,78 +2,59 @@ import triangle from "a-big-triangle";
 import createShader from "gl-shader";
 import createFBO from "gl-fbo";
 import loop from "raf-loop";
-import loadImage from "load-img";
-import demoImg from "../img/demo.jpg";
+import texture2D from "gl-texture2d";
 import vert from "./vert.glsl";
 import frag from "./frag.glsl";
 
-let gl: WebGL2RenderingContext;
+export default class Gaussian {
+  private blurRadius = 1;
+  private gl: WebGL2RenderingContext;
+  private shader: typeof createShader;
+  private texture: typeof texture2D;
 
-const getBase64FromImageUrl = (url: string): Promise<string> => {
-  const img = new Image();
-  img.setAttribute("crossOrigin", "anonymous");
-  img.src = url;
+  constructor(canvas: HTMLCanvasElement, img: HTMLImageElement) {
+    this.gl = canvas.getContext("webgl2")!;
 
-  return new Promise((resolve) => {
-    img.onload = function onload() {
-      const { height, width } = img;
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
+    const width = this.gl.drawingBufferWidth;
+    const height = this.gl.drawingBufferHeight;
+    const texture = texture2D(this.gl, img);
 
-      const offscreenCanvas = document.createElement("canvas");
-      gl = offscreenCanvas.getContext("webgl2")!;
-      offscreenCanvas.width = width;
-      offscreenCanvas.height = height;
-      canvas.getContext("2d")!.drawImage(img, 0, 0);
+    const shader = createShader(this.gl, vert, frag);
+    shader.bind();
+    shader.uniforms.iResolution = [width, height, 0];
+    shader.uniforms.iChannel0 = 0;
 
-      document.body.appendChild(offscreenCanvas);
-      return resolve(canvas.toDataURL("image/png"));
+    this.fboA = createFBO(this.gl, [width, height]);
+    this.fboB = createFBO(this.gl, [width, height]);
+
+    // apply linear filtering to get a smooth interpolation
+    const textures = [texture, this.fboA.color[0], this.fboB.color[0]];
+    const setParameters = (texture: typeof texture2D) => {
+      texture.wrapS = texture.wrapT = this.gl.REPEAT;
+      texture.minFilter = this.gl.LINEAR;
+      texture.magFilter = this.gl.LINEAR;
     };
-  });
-};
+    textures.forEach(setParameters);
+    this.texture = texture;
+    this.shader = shader;
+  }
 
-getBase64FromImageUrl(demoImg).then((uri) => {
-  loadImage(uri, start);
-});
+  setBlur(radius: number) {
+    this.blurRadius = radius;
+  }
 
-function start(err, image) {
-  if (err) throw err;
-
-  const width = gl.drawingBufferWidth;
-  const height = gl.drawingBufferHeight;
-  const texture = require("gl-texture2d")(gl, image);
-
-  const shader = createShader(gl, vert, frag);
-  shader.bind();
-  shader.uniforms.iResolution = [width, height, 0];
-  shader.uniforms.iChannel0 = 0;
-
-  const fboA = createFBO(gl, [width, height]);
-  const fboB = createFBO(gl, [width, height]);
-
-  // apply linear filtering to get a smooth interpolation
-  const textures = [texture, fboA.color[0], fboB.color[0]];
-  textures.forEach(setParameters);
-
-  let time = 0;
-
-  loop(render).start();
-
-  function render(dt: number) {
-    time += dt / 1000;
-    gl.viewport(0, 0, width, height);
-
-    const anim = Math.sin(time) * 0.5 + 0.5;
-    const iterations = 8;
-    let writeBuffer = fboA;
-    let readBuffer = fboB;
+  draw(iterations: number) {
+    let {
+      fboA: writeBuffer,
+      fboB: readBuffer,
+      blurRadius: radius,
+      texture,
+      shader,
+      gl,
+    } = this;
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     for (let i = 0; i < iterations; i += 1) {
-      // we will approximate a larger blur by using
-      // multiple iterations starting with a very wide radius
-      const radius = (iterations - i - 1) * anim;
-
       // draw blurred in one direction
       writeBuffer.bind();
       if (i === 0) {
@@ -100,11 +81,5 @@ function start(err, image) {
     shader.uniforms.direction = [0, 0]; // no blur
     shader.uniforms.flip = iterations % 2 !== 0;
     triangle(gl);
-  }
-
-  function setParameters(texture) {
-    texture.wrapS = texture.wrapT = gl.REPEAT;
-    texture.minFilter = gl.LINEAR;
-    texture.magFilter = gl.LINEAR;
   }
 }
