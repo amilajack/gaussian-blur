790e90b640858bace1166b4787f4e728fe4972a1
Amila Welihinda <amilajack@gmail.com>
2021-03-10 17:22:15 -0800

    feat: misc fixes

--- demo/app.ts
+++ demo/app.ts
@@ -1,5 +1,6 @@
 import Gaussian, { getBase64FromImageUrl } from "../src";
 import imgSrc from "../img/demo.jpg";
+import loop from 'raf-loop';
 
 (async () => {
   const canvas = document.querySelector("canvas")!;
@@ -13,17 +14,32 @@ import imgSrc from "../img/demo.jpg";
   });
 
   const blur = new Gaussian(canvas, img);
-  blur.draw(12);
+  blur.draw(2);
 
   const slider = document.querySelector("input") as HTMLInputElement;
   const output = document.getElementById("output") as HTMLInputElement;
   let blurRadius = 0;
   output.innerHTML = `Blur radius: ${blurRadius}`;
 
+  let speed = 1;
   slider.oninput = () => {
-    blurRadius = Number(slider.value) * 0.1;
-    output.innerHTML = `Blur radius: ${Math.round(blurRadius * 10) * 0.1}`;
-    blur.setBlur(parseInt(blurRadius));
-    blur.draw(12);
+    speed = slider.value
   };
+
+  window.onresize = () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    blur.gl.viewport(0,0,canvas.width, canvas.height)
+  }
+
+  let time = 0;
+
+  function render(dt: number) {
+    time += dt * speed / 1000;
+    const anim = Math.sin(time) * 0.5 + 0.5;
+    const iterations = 8;
+    blur.draw(iterations, anim);
+  }
+
+  loop(render).start();
 })();
